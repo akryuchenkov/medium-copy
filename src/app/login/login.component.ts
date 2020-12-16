ec0b2314cdbe0dc361e96b69e5c6c43a93d833ea
PokemonNelegal <alexandr.rdn@yandex.ru>
2020-12-16 21:00:25 +0300

    0

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+ 
+import {Component, OnInit} from '@angular/core';
+import {Router, Routes} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string = "";
+  password: string = "";
+  isInvalid: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
   }
 
+  ngOnInit() {
+  }
+
+  handleLogin() {
+    if (this.username === "admin" && this.password === "1234") {
+      this.isInvalid = false;
+      this.router.navigate(['welcome', this.username]);
+    } else {
+      this.isInvalid = true;
+    }
+    console.log(this.username)
+  }
 }
